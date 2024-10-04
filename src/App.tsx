eef651528e7c263c46a6f67a62fe43c539f3c2d6
Cyrus Lorenzo <cyruslorenzo0626@gmail.com>
2024-10-04 14:34:37 -0700

    Designed FIXED HERO SECTION and closer to the web inspiration design

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import {InfiniteMovingCardsDemo} from "./components/InfiniteMovingCardsDemo"
 
 function App() {
   return (
-    // Background 
+    // Background  s
     // My screen was unscrollable because I set in as h-screen
     //which sets the height of the entire app container to the full height of the screen. 
     //to fix the issue, you can try replacing h-screen with min-h-screen
@@ -27,7 +27,7 @@ function App() {
       fill="white"
       />
         {/* Main Container */}
-        <div className="h-50  p-4 mx-auto relative z-10 w-full pt-[100px] pb-[100px] md:pt-9 md:pb-0 px-2 ">
+        <div className="h-50  p-4 mx-auto relative z-10 w-full pt-[100px] pb-[100px] md:pt-20 md:pb-20 px-2 ">
 
           {/* Shiny Text */}
           {/* <div className="pb-5">
@@ -37,27 +37,39 @@ function App() {
           
           {/* Header Design */}
           {/* OHH SHI its the normal {text-4xl} that controls the font */}
-          <div className=" font-Inter font-bold text-[37px] leading-none md:text-7xl 
+          <div className=" font-Inter font-bold text-[45px] leading-none md:text-7xl 
           mt-5  md:mt-2  px-6 text-center 
           bg-clip-text text-transparent 
           bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-            Create, Grow and <br/> Scale your business
+            {/* Create, Grow and <br/> Scale your business */}
+            Beautiful. Fast. Private. <br/>Your Browser, Your Way
           </div>
 
 
-            <p className="mt-4 text-sm md:text-base sm:text-xs font-Poppins
-             text-neutral-300 max-w-sm text-center mx-auto px-4" >
-                Custom tailored solutions for your business.
+            <p className="mt-10 text-base md:text-base sm:text-xs font-Poppins
+             text-neutral-300 max-w-[400px] md:max-w-[640px] text-center mx-auto px-4" >
+                Beautifully designed, privacy-focused, and packed with features.
+                We care about your experience, not your data.
                  
             </p>
             
             <a href="/book"
+            className="cursor pointer mt-10
+            flex items-center bg-white
+            justify-center border rounded-full 
+            w-48 p-2 mx-auto my-6 text-black
+            ">
+              Book a Call ⤵
+            </a>
+
+            <a href="/book"
             className="cursor pointer 
             flex items-center
-            justify-center border rounded-full 
-            w-48 p-2 mx-auto my-6 text-white
+            font-Inter
+            justify-center 
+            w-48  mx-auto my-6 text-white
             ">
-              Book a Call
+              Start Exploring
             </a>
             
             
